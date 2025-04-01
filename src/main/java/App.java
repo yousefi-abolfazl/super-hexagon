ee446ad3336f1c72bbb8308411a36c35340ff0d9
Abolfazl Yousefi <abolfazlyousefi2004@gmail.com>
2025-04-01 18:10:55 +0330

    Implement project structure

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import ui.MainMenu;
+
+public class App {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.setVisible(true);
+        });
+    }
+}
